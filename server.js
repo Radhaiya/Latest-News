2d684ba5443dfb0a03c5b5db1d01f168a9a65037
Radhaiya <kulkarnir27999@gmail.com>
2022-12-30 23:47:34 +0530

    Deploy test

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const routes = require('./routes/routes')
+// const routes = require('./routes/routes')
 
-app.use('/', routes);
+// app.use('/', routes);
 
 app.get('/home', (req, res) => {
     res.send('Welcome to Indian Latest News API')
