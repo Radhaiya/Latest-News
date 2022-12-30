5db2817ed09902001d202e2a1b9eae69bbf2c28d
Radhaiya <kulkarnir27999@gmail.com>
2022-12-30 23:18:29 +0530

    New Route

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ const Data = require('./functions');
 const newsLetters = require('./newsLetterData');
 const cache = require('memory-cache');
 
-route.get('/latestnews', async (req, res) => {
+route.get('/', async (req, res) => {
 
 
     const Articles = [];
@@ -27,6 +27,33 @@ route.get('/latestnews', async (req, res) => {
     return res.send(Articles);
 
 
+})
+
+route.get('/:newsletter', async (req, res) => {
+    const newsLetter = req.params.newsletter
+    const Articles = [];
+    const TimesOfIndia_url = `${newsLetters[0].address}`;
+    const HindustanTimes_url = `${newsLetters[1].address}`
+    const NDTV_url = `${newsLetters[2].address}`
+
+
+    switch (newsLetter) {
+        case 'timesofindia':
+            const response1 = await axios.get(TimesOfIndia_url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+            Data.TimesOfIndia(Articles, response1.data);
+
+            return res.send(Articles);
+        case 'hindustantimes':
+            const response2 = await axios.get(HindustanTimes_url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+            Data.HindustanTimes(Articles, response2.data);
+            return res.send(Articles);
+        case 'ndtv':
+            const response3 = await axios.get(NDTV_url, { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+            Data.NDTV(Articles, response3.data);
+            return res.send(Articles);
+
+
+    }
 })
 
 
