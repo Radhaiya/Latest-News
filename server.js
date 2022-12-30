5859a13070fa574a04fd092e51811947c7347869
Radhaiya <kulkarnir27999@gmail.com>
2022-12-30 23:38:14 +0530

    changess

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const PORT = 8080;
 const routes = require('./routes/routes')
 
-app.use('/', routes);   
+app.use('/', routes);
 
-app.get('/', (req, res) => {
-    res.send('he')
+app.get('/home', (req, res) => {
+    res.send('Welcome to Indian Latest News API')
 })
 
-app.listen(PORT, () => {
-    console.log('Server Running');
+app.listen(3000, () => {
+    console.log('Server Running ');
 })
